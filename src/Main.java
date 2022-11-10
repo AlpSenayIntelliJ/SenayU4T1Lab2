7038ec4342b136e4e05e9286b56e873187462587
Alp Senay <alps2789@nycstudents.net>
2022-11-10 12:26:42 -0500

    working...

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        WhileLoopFun fun = new WhileLoopFun();
+
+        System.out.println("-- test printDigits --");
+        fun.printDigits(5);
+        System.out.println("---");
+        fun.printDigits(361);
+        System.out.println("---");
+        fun.printDigits(98000);
+        System.out.println("---");
+        fun.printDigits(150856);
+
+
+    }
 }
